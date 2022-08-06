753ee795da566c2253583da5303a2afcc6857be0
axel <axssel.diazfl@gmail.com>
2022-08-05 19:40:59 -0700

    adding color to the cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,35 @@
-import { StyleSheet, View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 import React from 'react';
+import { StyleSheet, View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 
 export default function Card(props) {
   const { character } = props;
+
+  const POKEMON_TYPE_COLORS = {
+    normal: "#A8A878",
+    fighting: "#C03028",
+    flying: "#A890F0",
+    poison: "#A040A0",
+    ground: "#E0C068",
+    rock: "#B8A038",
+    bug: "#A8B820",
+    ghost: "#705898",
+    steel: "#B8B8D0",
+    fire: "#FA6C6C",
+    water: "#6890F0",
+    grass: "#48CFB2",
+    electric: "#FFCE4B",
+    psychic: "#F85888",
+    ice: "#98D8D8",
+    dragon: "#7038F8",
+    dark: "#705848",
+    fairy: "#EE99AC",
+  };
+
+  const CharacterColor = (type) => POKEMON_TYPE_COLORS[type.toLowerCase()]
+
+  const Color = CharacterColor(character.type);
+
+  const background = { backgroundColor: Color, ...styles.backgroundStyles };
 
   const goToTheCharacter = () => {
     console.log(`go to the character ${character.name}`)
@@ -14,10 +41,10 @@ export default function Card(props) {
       <TouchableWithoutFeedback onPress={goToTheCharacter}>
         <View style={styles.card}>
           <View style={styles.spacing}>
-            <View style={styles.background}>
+            <View style={background}>
               <Text style={styles.number}>#{character.order}</Text>
               <Text style={styles.name}>{character.name}</Text>
-              <Image source={{ uri: character.imagen }} style={styles.image} />
+              <Image source={{ uri: character.image }} style={styles.image} />
             </View>
           </View>
         </View>
@@ -36,9 +63,6 @@ const styles = StyleSheet.create({
     padding: 5,
     width: 190,
   },
-  background: {
-    backgroundColor: "grey",
-  },
   image: {
     position: 'absolute',
     bottom: 2,
@@ -51,6 +75,12 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 15,
     paddingTop: 10,
+  },
+  backgroundStyles: {
+    flex: 1,
+    borderRadius: 15,
+    padding: 10,
+
   },
   number: {
     position: "absolute",
