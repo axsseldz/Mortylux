9b5902d6b25ca4df9d4e8401bb5bf0b4d8a70d7c
axel <axssel.diazfl@gmail.com>
2022-08-08 17:43:52 -0700

    [show pokemons in Favorites] feature added.

--- src/components/Saludar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from "react-native";
-
-export default function Saludar(props) {
-    const { name } = props;
-
-    return <Text>Hola {name}</Text>;
-}
--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,19 +1,57 @@
-import React from "react";
-import { SafeAreaView, Text, Button } from "react-native";
+import React, { useState, useCallback } from "react";
+import { Text } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import useAuth from "../hooks/useAuth";
 import { getCharactersFavoriteApi } from "../data/favorite";
+import CharacterList from "../components/CharacterList";
 
 export default function Favorite() {
+  const [characters, setCharacters] = useState([]);
+  const { auth } = useAuth();
 
-  const checkFavorites = async () => {
-    const response = await getCharactersFavoriteApi();
-    console.log(response);
 
+  async function getCharactersDetailsApi(id) {
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+      const response = await fetch(url);
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      throw error;
+    }
   }
 
-  return (
-    <SafeAreaView>
-      <Text>Favorite</Text>
-      <Button title="Get favorites" onPress={checkFavorites} />
-    </SafeAreaView>
+  useFocusEffect(
+
+    useCallback(() => {
+      if (auth) {
+        (async () => {
+          const response = await getCharactersFavoriteApi();
+
+          const charactersArray = [];
+          for await (const id of response) {
+            const characterDetails = await getCharactersDetailsApi(id);
+
+            charactersArray.push({
+              id: characterDetails.id,
+              name: characterDetails.name,
+              type: characterDetails.types[0].type.name,
+              order: characterDetails.order,
+              image:
+                characterDetails.sprites.other["official-artwork"].front_default,
+            });
+          }
+          setCharacters(charactersArray);
+        })()
+      }
+    }, [auth])
+  )
+
+
+
+  return !auth ? (
+    <Text>usuario no logeado</Text>
+  ) : (
+    <CharacterList characters={characters} />
   );
 }
